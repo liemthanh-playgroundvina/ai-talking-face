dd42fdcdb4f258df5ac6be21f1f0023039136bd0
Luong PHAM <luongpham@playgroundvina.com>
2024-07-05 04:31:42 +0000

    update queue

--- ai_celery/celery_app.py
+++ ai_celery/celery_app.py
@@ -11,4 +11,12 @@ app.config_from_object({
     ],
     'result_expires': 60 * 60 * 48,
     'task_always_eager': False,
+    'broker_transport_options': {
+        'visibility_timeout': 3600,
+        'max_retries': 3,
+        'interval_start': 0,
+        'interval_step': 0.2,
+        'interval_max': 0.5,
+    },
+    'worker_cancel_long_running_tasks_on_connection_loss': True,
 })
